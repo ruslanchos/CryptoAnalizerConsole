df9f053e324eb53c1ed6af7371de29f306409f98
Ruslan Bityutskih <ruslanchos@gmail.com>
2022-03-17 22:33:15 +0300

    003 Commit - add encoding

--- src/main/java/ru/javarush/bityutskih/cryptoanalizerconsole/ConsoleRunner.java
+++ src/main/java/ru/javarush/bityutskih/cryptoanalizerconsole/ConsoleRunner.java
@@ -1,7 +1,21 @@
 package ru.javarush.bityutskih.cryptoanalizerconsole;
 
+import java.util.List;
+import java.util.Locale;
+
 public class ConsoleRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello maven");
+    static String ALPHABET = "абвгдежзиклмнопрстуфхцчшщъыьэя";
+
+    public static String encoding(String printText, int Key) {
+        printText = printText.toUpperCase();
+        StringBuilder cryptoText = new StringBuilder();
+        for (int i = 0; i < printText.length(); i++) {
+            int charIndex = ALPHABET.indexOf(printText.charAt(i));
+            int newIndex = (charIndex + Key) % 30;
+            char cipherChar = ALPHABET.charAt(newIndex);
+            cryptoText.insert(0, cipherChar);
+        }
+        return cryptoText.toString();
+
     }
 }
